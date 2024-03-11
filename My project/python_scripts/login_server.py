4221a2ae7dfbd14e61f2d4223cac0a518641cac8
weedlysine <yjuu12345@naver.com>
2024-03-11 18:35:48 +0900

    3_11
    로그인 정보 확인 및 로그남기기
    aws db연동

--- My project/python_scripts/login_server.py
+++ My project/python_scripts/login_server.py
@@ -4,13 +4,21 @@ import pymysql
 import threading  # 스레드 모듈 추가
 import json
 import hashlib
+import string
+import random
+from datetime import datetime
 
 
 HOST = '127.0.0.1' 
-PORT = 8001       
+PORT = 8001
 
+DB_HOST=os.getenv('DB_HOST')
+DB_USERNAME=os.getenv('DB_USERNAME')
+DB_PASSWORD=os.getenv('DB_PASSWORD')
+DB_NAME=os.getenv('DB_NAME')
+DB_PORT=os.getenv('DB_PORT')      
 
-def func1():
+def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((HOST, PORT))
@@ -27,43 +35,72 @@ def func1():
     
     json_data = json.loads(data)
     print(json_data)
-    id = json_data["ID"]
-    password = json_data["password"]
-    print("Received data - ID:", id, "Password:", password)
-
-    identification = func2(id,password)
-    if identification == True:
-        res ="true"
-        client_socket.sendall(("true").encode())
-    else:
-        client_socket.sendall(("false").encode())
-        
+    type = json_data["Type"]
+    
+    if type == 'login':
+        identfy = identification(json_data)
+        if identfy == True:
+            client_socket.sendall(("true").encode())
+        else:
+            client_socket.sendall(("false").encode())
+    elif type == 'signup':
+        result = signup(json_data)
+        if result == 'success':
+            client_socket.sendall(("success").encode())
+        elif result == 'fail':
+            client_socket.sendall(("fail").encode())    
+    
     client_socket.close()
     server_socket.close()
-
-
-DB_HOST=os.getenv('DB_HOST')
-DB_USERNAME=os.getenv('DB_USERNAME')
-DB_PASSWORD=os.getenv('DB_PASSWORD')
-DB_NAME=os.getenv('DB_NAME')
-DB_PORT=os.getenv('DB_PORT')
-
-
-def func2(id,password):
+    
+def signup(data):
+    id = data["ID"]
+    password = data["password"]
+    name = data["name"]
     conn = pymysql.connect(host=DB_HOST,user=DB_USERNAME, password=DB_PASSWORD,database=DB_NAME,port=int(DB_PORT),cursorclass=pymysql.cursors.DictCursor)
     tmp_cursor =  conn.cursor()
-    sql_tmp = '''SELECT salt, Password FROM user WHERE ID = (%s)'''
+    sql_tmp = '''SELECT * FROM user WHERE ID = (%s)'''
     tmp_cursor.execute(sql_tmp,id)
     result = tmp_cursor.fetchall()
-    print(result)
+    if len(result) == 0:
+        print("해당아이디는 사용가능")
+        letters_set = string.ascii_lowercase + string.digits
+        salt = ''.join(random.sample(letters_set,10))
+        hash = str((hashlib.sha256((salt+password).encode())).hexdigest())
+        sql_tmp = '''INSERT INTO user (ID, Password, Name, salt) VALUES (%s, %s, %s, %s);'''
+        tmp_cursor.execute(sql_tmp,(id,hash,name,salt))
+        conn.commit()
+        return 'success'
+    else:
+        print("해당아이디는 사용불가")
+        return 'fail'
+    
+    
+
+def identification(data):
+    id = data["ID"]
+    password = data["password"]
+    conn = pymysql.connect(host=DB_HOST,user=DB_USERNAME, password=DB_PASSWORD,database=DB_NAME,port=int(DB_PORT),cursorclass=pymysql.cursors.DictCursor)
+    tmp_cursor =  conn.cursor()
+    sql_tmp = '''SELECT identification_number,salt, Password FROM user WHERE ID = (%s)'''
+    tmp_cursor.execute(sql_tmp,id)
+    result = tmp_cursor.fetchall()
+    if len(result) == 0:
+        return False
     salt = result[0]['salt']
     pwd = result[0]['Password']
     hash = hashlib.sha256((salt+password).encode())
     if pwd == str(hash.hexdigest()):
+        ip = data["ip"]
+        mac = data["MAC"]
+        sql_tmp = '''INSERT INTO login_db (LogIn_user, Time, login_ip, login_MAC) VALUES (%s, %s, %s, %s);'''
+        tmp_cursor.execute(sql_tmp,(result[0]['identification_number'],datetime.today().strftime("%Y%m%d%H%M%S") ,ip,mac))
+        conn.commit()
         return True
     else:
         return False
     
-    
-    
-func1()
+
+
+if __name__ == "__main__":
+    main()
